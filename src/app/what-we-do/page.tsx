e7aee4c8acda351a9325dd9e4f052023a693013e
Chetan Yadav <ydvchetan01@gmail.com>
2025-08-17 16:52:00 +0530

    what-we-do page

--- src/app/what-we-do/page.tsx
+++ src/app/what-we-do/page.tsx
@@ -1,7 +1,16 @@
-import React from "react";
+import Card from "@/components/whatwedo/Card";
 
 const WhatWeDo = () => {
-  return <div>WhatWeDo</div>;
+  return (
+    <main className="mt-36 text-center h-screen flex flex-col items-center">
+      <h2 className="text-6xl font-bold">What We Do</h2>
+      <p className="text-2xl text-gray-400 my-10 mx-auto">
+        We help developers master their coding skills through interactive
+        challenges, real-time practice, and comprehensive learning paths.
+      </p>
+      <Card />
+    </main>
+  );
 };
 
 export default WhatWeDo;
