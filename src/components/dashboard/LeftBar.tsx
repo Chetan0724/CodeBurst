dd6229024957896c14c79d1ba2ac13dadaa2e748
Chetan Yadav <ydvchetan01@gmail.com>
2025-12-10 22:55:03 +0530

    refactor LeftBar component to include toggle button for sidebar visibility

--- src/components/dashboard/LeftBar.tsx
+++ src/components/dashboard/LeftBar.tsx
@@ -2,20 +2,31 @@
 import LanguageDropdown from "./LanguageDropdown";
 import TaskCard from "./TaskCard";
 import { useContext } from "react";
+import { IconChevronLeft } from "@tabler/icons-react";
 import EditorContext from "@/context/EditorContext";
 import SidebarContext from "@/context/SidebarContext";
 
 const LeftBar = () => {
   const { tasks, setCurrTaskId } = useContext(EditorContext)!;
-  const { isSidebarOpen } = useContext(SidebarContext)!;
+  const { isSidebarOpen, setIsSidebarOpen } = useContext(SidebarContext)!;
 
   return (
     <aside
-      className={`bg-primarytwo  text-black dark:text-white border-r fixed left-0 top-[3.25rem] bottom-0 flex flex-col z-50 w-dvw sm:w-2xs transform transition-all duration-300 ease-in-out
-        ${isSidebarOpen ? "translate-x-0 opacity-100" : "-translate-x-full opacity-0"} `}
+      className={`bg-primarytwo  text-black dark:text-white border-r fixed left-0 top-0 bottom-0 flex flex-col z-50 w-dvw sm:w-2xs transform transition-all duration-300 ease-in-out
+        ${
+          isSidebarOpen
+            ? "translate-x-0 opacity-100"
+            : "-translate-x-full opacity-0"
+        } `}
     >
-      <div className="border-b shrink-0 p-3">
+      <div className="border-b shrink-0 p-3 flex gap-2 items-center">
         <LanguageDropdown />
+        <button
+          onClick={() => setIsSidebarOpen(!isSidebarOpen)}
+          className="cursor-pointer bg-black text-white dark:bg-white dark:text-black rounded-full w-5 h-5 flex items-center justify-center"
+        >
+          <IconChevronLeft stroke={2} />
+        </button>
       </div>
       <div className="space-y-2 flex-1 overflow-y-auto p-3">
         {tasks!.map(({ taskId, difficulty, topic, language }) => (
